b1890ad8985d16cbeba5fbb08f8b57569a9c1910
banueloslaraj-rgb <banueloslaraj@gmail.com>
2026-03-26 12:33:12 -0600

    Add files via upload

--- repartidor.js
+++ repartidor.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://TU_URL.supabase.co";
-const SUPABASE_KEY = "TU_ANON_KEY";
+const SUPABASE_URL = "https://pknqqaxiqdllsygjctmb.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbnFxYXhpcWRsbHN5Z2pjdG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NDY0MDEsImV4cCI6MjA5MDEyMjQwMX0.o3XrQk2xgN7F9qfHVVg1Ixz5ZYPQ_edZe9-jAENgiTc";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
